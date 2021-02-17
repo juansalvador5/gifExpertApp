9a973614024230a3096fbe232ed0b688aed63947
juansalvador5 <juanpersoglia@gmail.com>
2021-02-16 21:53:58 -0300

    feat(unitTests): test enviroment config

---
+++ src/tests/02-template-string.spec.js
@@ -0,0 +1,9 @@
+
+    it('Should be return message', ()=> {
+        const nombre = 'juansa';
+        const saludo = `Hola ${nombre}`;
+        
+        expect(saludo).toBe(`Hola ${nombre}`);
+
+    })
+
--- webpack.config.js
+++ webpack.config.js
@@ -53,6 +53,7 @@ module.exports = {
         ],
       }
     ],
+    
   },
   plugins: [
     new HtmlWebpackPlugin({
